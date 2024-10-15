5398ffaa7b309d348f0606bc2927b7b45d5f071f
YY T <changiyy07@gmail.com>
2024-10-15 19:37:20 +0800

    Removed comma from value col for all tables

--- data.sql
+++ data.sql
@@ -1,4 +1,6 @@
-select *
-from egg_production
-limit 10
-;
+UPDATE cheese_production SET value = REPLACE(value, ',', '');
+UPDATE honey_production SET value = REPLACE(value, ',', '');
+UPDATE milk_production SET value = REPLACE(value, ',', '');
+UPDATE coffee_production SET value = REPLACE(value, ',', '');
+UPDATE egg_production SET value = REPLACE(value, ',', '');
+UPDATE yogurt_production SET value = REPLACE(value, ',', '');
